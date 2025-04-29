0a0c704e47f6e1a980e7cc4cecc15b25f9605c8a
山田太郎 <>
2025-04-29 13:34:36 +0900

    next image

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   trailingSlash: true,
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
